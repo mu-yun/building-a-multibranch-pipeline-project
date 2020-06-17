3b05b7a8cd6199b956283584ebe1e605de139940
muyun <niu.muyun@gmail.com>
2020-06-17 18:06:25 +0800

    Update test.sh

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -13,7 +13,6 @@ set -x
 # npm install --save-dev cross-env
 set +x
 sleep 3
-echo 'The following "npm" command tests that your simple Node.js/React'
 echo 'application renders satisfactorily. This command actually invokes the test'
 set -x
 #npm test
