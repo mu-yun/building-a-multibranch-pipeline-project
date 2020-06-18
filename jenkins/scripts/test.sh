d67ad7c460b9679d3efb168f227304255ddf8b2a
muyun <niu.muyun@gmail.com>
2020-06-18 16:11:05 +0800

    Update test.sh

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env sh
 
 
-echo 'installing this dependency, it would typically be done so using this'
-echo 'flag. For a comprehensive explanation about "devDependencies", see'
 echo 'https://stackoverflow.com/questions/18875674/whats-the-difference-between-dependencies-devdependencies-and-peerdependencies.'
 set -x
 # npm install --save-dev cross-env
