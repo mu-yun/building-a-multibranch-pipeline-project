516092cd3186a9bbcc5cd89e56c83b415d04e32d
muyun <niu.muyun@gmail.com>
2020-06-18 16:15:19 +0800

    Update test.sh

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -3,7 +3,5 @@
 
 echo 'https://stackoverflow.com/questions/18875674/whats-the-difference-between-dependencies-devdependencies-and-peerdependencies.'
 set -x
-# npm install --save-dev cross-env
-set +x
-sleep 3
+
 
