a877d236bb14e3364ed42cbd5c0d79d9147485eb
muyun <niu.muyun@gmail.com>
2020-06-18 11:09:08 +0800

    Update test.sh

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env sh
 
 
-echo '"cross-env" dependency to be installed as "devDependencies". For the'
-echo 'purposes of this tutorial, this flag is not important. However, when'
 echo 'installing this dependency, it would typically be done so using this'
 echo 'flag. For a comprehensive explanation about "devDependencies", see'
 echo 'https://stackoverflow.com/questions/18875674/whats-the-difference-between-dependencies-devdependencies-and-peerdependencies.'
